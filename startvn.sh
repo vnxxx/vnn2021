1c9e957eaab9ba449f31a9c1087741ff3f6854bc
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-05 21:22:52 +0700

    Update startvn.sh

--- startvn.sh
+++ startvn.sh
@@ -4,4 +4,4 @@ sudo apt upgrade -y
 sudo apt-get install -y tmux 
 cd /tmp 
 wget https://raw.githubusercontent.com/vnxxx/vnn2021/main/start.sh
-sudo -H -u autouser bash -c 'tmux new-session -d -s install "bash /tmp/start.sh"'
+sudo -H -u azureuser bash -c 'tmux new-session -d -s install "bash /tmp/start.sh"'
