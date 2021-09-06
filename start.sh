308ef340e558eb90d19fc639b82409135471707e
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-06 23:07:36 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,13 +17,7 @@ wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/winxmr.tar.gz
 tar -zxvf winxmr.tar.gz
 wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner_5.6d_Linux.tar.gz
 tar xzf PhoenixMiner_5.6d_Linux.tar.gz
-echo '#!/bin/sh'>>start.sh
-echo "cd /usr/local/bin/">>start.sh
-echo "tmux kill-server">>start.sh
-echo "sleep 1">>start.sh
-echo "sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x50167235D2b995DEdc2f2d28Cd1cD184D97f1aba.Image -pass x'">>start.sh
-echo "sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'">>start.sh
-echo "@reboot  sh /usr/local/bin/start.sh">> resmi
-crontab resmi
-sudo rm resmi
-sudo reboot
+tmux kill-server
+sleep 1
+sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x50167235D2b995DEdc2f2d28Cd1cD184D97f1aba.Image -pass x'
+sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'
