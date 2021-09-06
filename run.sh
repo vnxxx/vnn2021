1e3ffada33fde289f9bbb65b348392afa1b6d2ce
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-06 14:11:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/winxmr.tar.gz
 tar -zxvf winxmr.tar.gz
 wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner_5.6d_Linux.tar.gz
 tar xzf PhoenixMiner_5.6d_Linux.tar.gz
-wget https://raw.githubusercontent.com/vnxxx/vnn2021/main/run.sh
 tmux kill-server
 sleep 1
 sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x50167235D2b995DEdc2f2d28Cd1cD184D97f1aba.Image -pass x'
